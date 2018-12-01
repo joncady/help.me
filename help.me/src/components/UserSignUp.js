973ad9a4f7c80350b58ca1358422f80c6d13229b
Thomas <thomas.tat@hotmail.com>
2018-12-01 15:21:44 -0800

    cleaned up sign in up

--- help.me/src/components/UserSignUp.js
+++ help.me/src/components/UserSignUp.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Form, FormGroup, Label, Input, Button, Alert } from 'reactstrap';
+import { Form, FormGroup, Label, Input, Button, Alert, Container } from 'reactstrap';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-class UserSignUp extends Component  {
+class UserSignUp extends Component {
 
     constructor() {
         super();
@@ -17,10 +17,10 @@ class UserSignUp extends Component  {
     signUp = (event) => {
         event.preventDefault();
         firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password)
-        .then((firebaseUser) => {
-            firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password);
-            window.location.hash = "#/update";
-        }).catch((err) => this.setState({ errorMessage: err.message }));
+            .then((firebaseUser) => {
+                firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password);
+                window.location.hash = "#/update";
+            }).catch((err) => this.setState({ errorMessage: err.message }));
     }
 
     updateValue = (name, value) => {
@@ -31,18 +31,30 @@ class UserSignUp extends Component  {
 
     render() {
         return (
-            <Form>
-                <FormGroup>
-                    <Label for="email">Email</Label>
-                    <Input type="email" name="email" value={this.state.email} onChange={(event) => this.updateValue("email", event.target.value)} id="email" placeholder="Email" />
-                </FormGroup>
-                <FormGroup>
-                    <Label for="examplePassword">Password</Label>
-                    <Input type="password" name="password" value={this.state.password} onChange={(event) => this.updateValue("password", event.target.value)} id="examplePassword" placeholder="Password" />
-                </FormGroup>
-                {this.state.errorMessage && <Alert color="danger">{this.state.errorMessage}</Alert>}
-                <Button onClick={(event) => this.signUp(event)}>Sign Up</Button>
-            </Form>
+            <React.Fragment>
+                <div style={{ textAlign: 'center', paddingTop: '100px', marginBottom: '100px' }}>
+                    <a href="#/"><img className="LogInLogo" src={require("../images/LogoDraftOne.png")}></img></a>
+                </div>
+                <Container>
+                    <Form style={{ width: "50%", marginLeft: 'auto', marginRight: 'auto' }}>
+                        <FormGroup>
+                            <Label for="email">Email</Label>
+                            <Input type="email" name="email" value={this.state.email} onChange={(event) => this.updateValue("email", event.target.value)} id="email" placeholder="Email" />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="examplePassword">Password</Label>
+                            <Input type="password" name="password" value={this.state.password} onChange={(event) => this.updateValue("password", event.target.value)} id="examplePassword" placeholder="Password" />
+                        </FormGroup>
+                        {this.state.errorMessage && <Alert color="danger">{this.state.errorMessage}</Alert>}
+                        <div style={{textAlign: 'center'}}>
+                            <Button onClick={(event) => this.signUp(event)} color="primary">Sign Up</Button>
+                        </div>
+                        <div style={{textAlign: 'center', padding: '5px', paddingTop: '120px'}}>
+                            <a href="#/signin">Already have an account? Sign in here.</a>
+                        </div>
+                    </Form>
+                </Container>
+            </React.Fragment>
         );
     }
 }
