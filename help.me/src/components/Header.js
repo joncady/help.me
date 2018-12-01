55f7e0de200bce234de8098c54e07cda7131e6c6
Thomas <thomas.tat@hotmail.com>
2018-12-01 13:05:49 -0800

    basic front end

--- help.me/src/components/Header.js
+++ help.me/src/components/Header.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { Button } from 'reactstrap';
 
 class Header extends Component {
 
     render() {
         return(
-            <header></header>
+            <header>
+                <div id="change">
+                    <img id ="logo" src={require("../images/LogoDraftOne.png")}/>    
+                    <div>
+                        <Button className="button" color="success" id="SignUpButton">Sign Up</Button>
+                        <Button outline color="success" id="SignUpButton">Sign In</Button>
+                    </div>
+                </div>
+            </header>
         )
     }
 }
