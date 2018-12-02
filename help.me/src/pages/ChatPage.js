8fb8af82b6b8e5a4c216c6d03f283338f13a8f92
Thomas <thomas.tat@hotmail.com>
2018-12-01 16:11:49 -0800

    chat

--- help.me/src/pages/ChatPage.js
+++ help.me/src/pages/ChatPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Jumbotron, Button } from 'reactstrap';
+import { Jumbotron, Button, Input, InputGroup } from 'reactstrap';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
@@ -7,7 +7,22 @@ class ChatPage extends Component {
 
     render() {
         return (
-            <h1>hi</h1>
+            <React.Fragment>
+            <header>
+                <div id="change">
+                    <a href="/"><img id ="logo" src={require("../images/LogoDraftOne.png")} alt="Logo"/></a>    
+                    <div>
+                        <Button color="success" onClick={() => this.props.set("signUp")}>Back To Home</Button>
+                    </div>
+                </div>
+            </header>
+            <main>
+                <InputGroup>
+                    <Input placeholder="Enter your message here..." />
+                </InputGroup>
+                <Button color="success">SEND</Button>
+            </main>
+            </React.Fragment>
         )
     }
 
