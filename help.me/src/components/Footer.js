9f6f4a816e4aeb34df4b3f3da2520613fae1fc14
popi1141 <popi1141@uw.edu>
2018-12-01 17:40:51 -0800

    Update Footer.js

--- help.me/src/components/Footer.js
+++ help.me/src/components/Footer.js
@@ -6,7 +6,7 @@ class Footer extends Component {
         return(
             <footer style={{ display: 'flex', justifyContent: 'space-between' }}>
                 <div style={{ padding: '1em' }}>
-                    <p className="footer-text">PROJECT BY -</p>
+                    <p className="footer-text">PROJECT BY JONATHAN CADY, THOMAS THAT, JUSTIN BANUSING, JOHN KIM </p>
                 </div>
                 <div style={{ padding: '1em' }}>
                     <a href="/#">ABOUT</a> | <a href="/#">DOCUMENTATION</a> | <a href="/#">CONTACT US</a>
